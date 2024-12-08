019c34ed16419c29aea672280bf644a1e26f1fd9
Roman <108236042+romanbaba@users.noreply.github.com>
2024-12-08 22:12:51 +0300

    ☁️ added `server connection`

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -12,7 +12,7 @@ export default function Page() {
 			<div className={cn("flex items-center gap-x-2 rounded-full px-3 py-1.5", isLoading ? "bg-neutral-500/10  text-neutral-500" : data ? "bg-green-500/10  text-green-500" : "bg-red-500/10  text-red-500")}>
 				<div className={cn("size-3 rounded-full", isLoading ? "bg-neutral-500" : data ? "bg-green-500" : "bg-red-500")}></div>
 				<span className={cn("text-sm", isLoading ? "text-neutral-500" : data ? "text-green-500" : "text-red-500")}>
-					{isLoading ? "Bağlanılıyor..." : data ? "Bağlandı." : "Bağlanamadı."}
+					{isLoading ? "Bağlanılıyor..." : data ? "Bağlandı." : "Bağalanılamadı."}
 				</span>
 			</div>
 		</div>
