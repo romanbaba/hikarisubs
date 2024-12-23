6160ae1e75ce3bb39fa3a8e1c249ac9262eb524d
Roman <108236042+romanbaba@users.noreply.github.com>
2024-12-23 21:24:57 +0300

    ⬆️ Updated Header

--- client/src/app/components/button.jsx
+++ client/src/app/components/button.jsx
@@ -1,9 +1,11 @@
 // @ts-check
+import cn from "@/functions/cn";
+import { Loader2 } from "lucide-react";
 import { signIn, signOut } from "next-auth/react";
-import React from "react";
+import React, { useState } from "react";
 import { FaGoogle } from "react-icons/fa";
 
-/** @param {{ children: React.ReactNode } & React.HTMLAttributes<HTMLButtonElement>} args */
+/** @param {{ children: React.ReactNode } & React.ButtonHTMLAttributes<HTMLButtonElement>} args */
 export default function Button({ children, ...args }) {
 	return (
 		<button {...args}>{children}</button>
@@ -25,13 +27,20 @@ Button.SignIn = function ButtonSignIn({ children, ...args }) {
 
 /** @param {{ children: React.ReactNode } & React.HTMLAttributes<HTMLButtonElement>} args */
 Button.SignIn = function ButtonSignIn({ children, ...args }) {
+	const [ isAuthorizing, setAuthorizing ] = useState(false);
+	const handleChange = () => {
+		signIn("google");
+		setAuthorizing(!isAuthorizing);
+	};
+
 	return (
 		<Button
-			onClick={() => signIn("google")}
-			className="px-4 py-1.5  flex items-center justify-center gap-x-2 rounded-full text-black bg-white text-sm font-semibold hover:bg-white/70 transition-colors"
+			disabled={isAuthorizing}
+			onClick={handleChange}
+			className={cn("px-4 py-1.5 flex items-center justify-center gap-x-2 rounded-full text-black bg-white text-sm font-semibold hover:bg-white/70 transition-all", isAuthorizing && "bg-white/70 cursor-not-allowed")}
 			{...args}
 		>
-			<FaGoogle /> {children}
+			{isAuthorizing ? <Loader2 size={14} className="animate-spin font-semibold" /> : <FaGoogle />} {children}
 		</Button>
 	);
 };
@@ -41,7 +50,6 @@ Button.SignOut = function ButtonSignOut({ children, ...args }) {
 	return (
 		<Button
 			onClick={() => signOut()}
-			className="px-4 py-1.5 flex items-center justify-center gap-x-2 rounded-full text-white bg-[#e5484d] text-sm font-semibold hover:bg-[#e5484d]/70 transition-colors"
 			{...args}
 		>
 			{children}
--- client/src/app/components/header.jsx
+++ client/src/app/components/header.jsx
@@ -1,13 +1,65 @@
+// @ts-check
 import Button from "@/app/components/button.jsx";
-import Input from "./input";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Header() {
 	return (
-		<div className="mt-3">
-			<div className="flex w-full gap-x-2 justify-between items-center">
-				<Input.Search />
-				<Button.SignIn>Google ile Oturum aç</Button.SignIn>
+		<div className="pt-[0.313rem]">
+			<div className="py-[0.8rem] px-[1rem]">
+				<div className="flex w-full gap-x-2 justify-between items-center">
+					<div></div>
+					<UserSide />
+				</div>
 			</div>
 		</div>
+	);
+}
+
+function UserSide() {
+	const { status, data } = useSession();
+
+	return (
+		<>
+			{status === "authenticated" ? (
+				<div className="flex flex-row items-center justify-center space-x-5">
+					<Image
+						src={data.user.image}
+						alt={data.user.name}
+						className="rounded-full"
+						height={40}
+						width={40}
+					/>
+
+
+					<div className="flex flex-col items-start">
+						<h2 className="text-xs text-[#9999a5]">Merhaba,</h2>
+						<DropdownMenu.Root>
+							<DropdownMenu.Trigger asChild>
+								<button className="focus:outline-none text-sm">
+									{data.user.name}
+								</button>
+							</DropdownMenu.Trigger>
+
+							<DropdownMenu.Portal>
+								<DropdownMenu.Content className="bg-[#161616] border border-white/5 mt-3 w-48 p-2 space-y-2 rounded-xl" sideOffset={5}>
+									<DropdownMenu.Item className="text-sm hover:outline-none">
+										Profil
+									</DropdownMenu.Item>
+
+
+									<DropdownMenu.Item className="text-sm hover:outline-none text-red-500 hover:text-red-500/70 transition-all">
+										<Button.SignOut>Çıkış yap</Button.SignOut>
+									</DropdownMenu.Item>
+								</DropdownMenu.Content>
+							</DropdownMenu.Portal>
+						</DropdownMenu.Root>
+					</div>
+				</div>
+			) : (
+				<Button.SignIn>Google ile Oturum aç</Button.SignIn>
+			)}
+		</>
 	);
 }
